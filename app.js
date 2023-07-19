8ad6551f0fa5c6f4384a61e17dbcc1f7cdfa14b1
Sweedsiter <d27saitunlu@gmail.com>
2023-07-19 18:28:53 +0700

    3addData

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const POST = 4001
+const uri = "mongodb+srv://d27saitunlu:cQ8ppx22JYaohtq1@cluster0.mtjpud0.mongodb.net/productDB2";
 
 app.listen(POST,()=>{
     console.log("Start POST 4001")
@@ -11,7 +12,7 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/product',(req,res)=>{
-    res.send("this is Product 4001")
+    res.json(uri)
 })
 
 module.exports = app
